6f9c27693008ff7abbea31dde76066ac2a6f14a4
Diego Silva <diegosiuniube@gmail.com>
2017-09-12 15:37:06 -0300

    fixes

--- docker.sh
+++ docker.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-export BUILD_NUMBER=01
-docker build --no-cache=true -f Dockerfile -t diegosiuniube/grpc-server:1.0 .
+./gradlew installDist && docker build --no-cache=true -f Dockerfile -t diegosiuniube/grpc-server:1.0 .
